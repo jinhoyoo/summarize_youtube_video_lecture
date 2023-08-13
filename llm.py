6be3370f3b248e5b5b148543b4b598150a53f7c5
jinhoyoo <brotherjinho@gmail.com>
2023-08-13 16:54:10 +0900

    initial code
    WIP
    
    WIP

--- llm.py
+++ llm.py
@@ -5,25 +5,22 @@ from dotenv import load_dotenv
 
 def correct_text(text, max_token=3000, model="gpt-3.5-turbo", language="ko"):
 
+    prompt = {
+        "ko":"아래 문장의 문장부호를 다 정리해줘. 그리고 소리나는대로 잘못 적은 단어를 고쳐줘. 내용상 나뉘어야 하면 줄바꿈도 해줘.", 
+        "en":"Please organize the punctuation marks in the following sentences. Also, correct any incorrectly written words as you hear them. If there should be divisions in content, please include line breaks"
+    }
+
     positioned_command = {
         "ko": "한국어로 교정한 글들",
         "en": "Corrected text"
     }
 
-    prompt = f"""
-    Maintain the given language accordingly and correct all the typos and rectify any inaccuracies in names or locations in the given text.
+    prompt = \
+f"""
+{prompt[language]}
 
-    these are the examples for correction.
-
-    태러다임 -> 패러다임 
-    어보이친사 -> 어버이 친 자
-    규족 -> 귀족 
-
-    ----------
-    {text}
-
-    {positioned_command[language]}:
-    """
+{text}
+"""
     
     time.sleep(0.5) # Avoid the bad request error. 
     response = openai.ChatCompletion.create(
@@ -47,48 +44,18 @@ def summarize_text(text, max_token=3000, model="gpt-3.5-turbo", language="ko"):
     }
         
     prompt = f"""
-        Summarize the text below as well-organized note.
-        ----------
-        {text}
 
-        {positioned_command[language]}:
-        """
-
-    time.sleep(0.5) # Avoid the bad request error. 
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=[
-            {"role": "system", "content": "You are a sophmore student."},
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=max_token
-    )
-
-    corrected_text = response.choices[0].message.content
-    return corrected_text
-
-
-
-def rectify_and_summarize_text(text, max_token=3000, model="gpt-3.5-turbo", language="ko"):
-
-    positioned_command = {
-        "ko": "한국어로 요약한 글",
-        "en": "Summary note"
-    }
-        
-    prompt = f"""
-Ensure adherence to the provided language, rectify any typographical errors, and then create a well-structured summary of the following text with bullet points.
+    아래 글들을 노트로 정리해줘. 중요한건 -표시로 Markdown처럼 아래처럼 구조적으로 정리해줘. 그리고 정리한 내용과 원문을 다시 보고, 중요한건 -표시로 Markdown처럼 구조적으로 정리해줘. 가장 마지막에 정리한 것만 출력해줘.
 ----------
 {text}
 
-{positioned_command[language]}:
         """
 
     time.sleep(0.5) # Avoid the bad request error. 
     response = openai.ChatCompletion.create(
         model=model,
         messages=[
-            {"role": "system", "content": "You are a sophmore student."},
+            {"role": "system", "content": "You are a university student."},
             {"role": "user", "content": prompt}
         ],
         max_tokens=max_token
@@ -96,6 +63,67 @@ Ensure adherence to the provided language, rectify any typographical errors, and
 
     corrected_text = response.choices[0].message.content
     return corrected_text
+
+
+
+def rectify_note_with_summarized_note_and_original_text(note, text, max_token=3000, model="gpt-3.5-turbo", language="ko"):
+
+ 
+    prompt = f"""
+아래 글을 원문이야. 
+----------
+{text}
+
+아래 글은 위의 글을 바탕으로 정리한거야. 
+----------
+{note}
+
+위에 글과, 아래글을 보고 잘 다시 핵심만 노트로 정리해줘. 중요한건 -표시로 Markdown처럼 아래처럼 구조적으로 정리해줘.
+
+text
+- sub text
+- sub text 
+....
+
+
+"""
+
+    time.sleep(0.5) # Avoid the bad request error. 
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a university student."},
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=max_token
+    )
+
+    corrected_note = response.choices[0].message.content
+    return corrected_note
+
+
+def add_symbols_and_fix_errata(text, max_token=3000, model="gpt-3.5-turbo", language="ko"):
+
+ 
+    prompt = f"""
+아래 문장의 문장부호를 다 정리해줘. 그리고 소리나는대로 잘못 적은 단어를 고쳐줘. 내용상 나뉘어야 하면 줄바꿈도 해줘.
+----------
+{text}
+
+"""
+
+    time.sleep(0.5) # Avoid the bad request error. 
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a university student."},
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=max_token
+    )
+
+    corrected_note = response.choices[0].message.content
+    return corrected_note
 
 
 def main():
@@ -103,22 +131,21 @@ def main():
     # Setup OpenAI API key 
     load_dotenv()
 
-    text = """
-        이것은 철학적. 이것은 그래서 태러다임이라고도 말하고 Basic Assumption, 기본전제예요. 우리가 세계를 이해하는 기본전제가 자기한테 있거든요. 윤석열이 말하는 걸 보면 기본전제가 뭐라는 걸 금방 알 수 있지 않습니까? 기본전제가 뭐예요? 친일파예요. 친일적 생각을 하고 있다.그냥 친일을 하는 건 일본하고 친하게 지진하는 지는 지는 자기가 아니에요. 역사학자 존용 선생님이 말해 보면 그 친자는 어보이친사라는 거.그래서 친일이라는 말은 그냥 일본하고 친하게 지진다는 게 아니라 일본을 어보이로 성기겠다는 뜻이란 말이죠. 실제로 그렇게 성긴 사람들이 나중에 뭐예요? 다 규족, 자기를 받고 어마어마한 재산을 하사받고 총독으로부터 하사받고 그러지 않습니까? 
 
-        튼튼한 철학적 기반을 어디서 어디서 얻을 수 있을 거예요. 대학에서 얻는 겁니다. 그래서 대학은 입문하기에 중요한 거예요. 철학적 사유가 없이는 그걸 기반으로 이론을 만들어낼 수가 없기 때문입니다. 이론이 만들어내봐야 미국 사람들이 만들어낸 거 우리가 외워서 뱉여다가 여기서 그냥 대학에서 가르치는 그런 이론 밖에 나올 수 있는 게 없어요. 우리가 만들어낸 철학에서 우리가 이론을 철학에 바탕으로 이론을 만들어내서 그걸 가지고 우리가 서로 토론하고 더 좋은 것이 뭔지를생각해낼 수 있어야 되잖아요. 그래서 제가 성취해충 모형이라는 책을 이론적으로 썼습니다. 방법만을 쓴 거죠. 철학이 있으면 그걸 가지고 이론을 만들어내야 되는데 이론은 되게 방법론을 가지고 있어요. 그래서 방법론이 없으면 이론이라기 보기 어렵죠. 그건 철학이나 마찬가지죠. 그러니까 이론에는 반드시 방법론이 있어야 되는데 그 방법론을 가지고 구체적인 수단과 도구를 만들어내는 겁니다. 구체적인 수단과 도구를 가지고 현실에서 쓰는 거죠. 집행하는 겁니다. 
+    text = """
+제가 그 자료를 찾다가 시간이 없어서 그냥 가져왔는데 독일을 보면 알아요. 독일이 뭐냐면 정부 부채가 국가부채죠. 정부 부채가 약 GDP의 80%까지 올라갔었어요. 왜냐하면 시리아 난민을, 독일 얘기입니다. 2015년에 시리아 난민을 대거 들어왔잖아요. 100만 명 넘게 들어오는 바람에 그거에 대한 재정 지출을 하지 않을 수 없었잖아요. 그러니까 부채 비율이 70%, 80%까지 올라갔다가 그것을 점점 낮춰서 약 40%까지 낮췄었어요. 그런데 2019년이 어떻게 됐어요? 코로나 사태가 불거졌잖아요. 그러니까 코로나 때문에 자영업자들에 대한 생계를 보존해 주기 위해서 문 닫아도, 가게 문을 닫아도 다 그 수입을 보존해 줬어요. 완벽하게 보존해 줬죠. 그때 메르켈 정부였잖아요. 메르켈 정부에서 부총리 겸 재무부 장관을 했었던 사람이 누구예요? 지금 총리 하는 올라프 숄츠예요. 그래서 이 민주주의에 있는 사람들이 계속 예찬을 포함한 많은 사람들이 계속 얘기했던 거예요. 재난지원금을 지급하라.
     """
 
-    # revised_text = correct_text(text)
-    # print(revised_text)
+    note = """
+- 문재인 정부가 막판에 재난지원금을 주지 않았다고 함.
+- 홍남기가 세수 추계를 잘못하여 추가적인 예산이 필요해짐.
+- 해당 예산으로 60조를 추가로 사용하였고, 만약 55조로 사용했다면 상황이 어땠을지 생각해봄.
+- 윤석열 등 일부 세력은 국가의 통치를 위협하는 존재로 여겨짐.
+    """
 
-    # print('/n')
+    rewritten_note = rectify_note_with_summarized_note_and_original_text(note, text)
+    print(rewritten_note)
 
-    # summary_note = summarize_text(revised_text)
-    # print(summary_note)
-
-    summary_note = rectify_and_summarize_text(text)
-    print(summary_note)
 
 if __name__ == "__main__":
     main()
